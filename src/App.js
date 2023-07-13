71050103336711c7cdae632b2ba6b7b8679ed43e
Nikitalambat <mailto:lambatnikita@gmail.com>
2023-07-14 00:11:51 +0530

    more than state use with style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm'
+// import TextForm from './components/TextForm'
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <Navbar title="Nitika" aboutText="About" />
       {/* <Navbar  /> */}
       <div className="container my-3">
-         <TextForm heading="Enter the text to analyze below"/>
+         {/* <TextForm heading="Enter the text to analyze below"/> */}
+         <About  />
       </div>
       
     </>
